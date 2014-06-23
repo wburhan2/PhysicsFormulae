9d0984fb4d1a7d98a66f19cee4e65005b0781cf2
Wilson Burhan <wilson.burhan@gmail.com>
2014-06-22 17:58:38 -0700

    Made the main activity to only contain a frame layout container, which will be replaced by list views. Main_menu->sub_menu->topic, etc..
    Added transition in between fragments.

--- app/src/main/java/com/physicsformulae/app/MainActivity.java
+++ app/src/main/java/com/physicsformulae/app/MainActivity.java
@@ -6,29 +6,12 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.google.inject.Inject;
 
 import roboguice.activity.RoboFragmentActivity;
-import roboguice.inject.InjectResource;
-import roboguice.inject.InjectView;
 
 public class MainActivity extends RoboFragmentActivity {
-
-    private final int KINEMATICS = 0;
-    private final int FORCES = 1;
-    private final int DYNAMICS = 2;
-    private final int WORK = 3;
-
-
-    @InjectView(R.id.list_view)
-    ListView mListView;
-
-    @InjectResource(R.array.topic_list)
-    String[] mListOfTopics;
 
     // system service
     @Inject
@@ -39,10 +22,11 @@ public class MainActivity extends RoboFragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final MyAdapter adapter = new MyAdapter(mListOfTopics);
-        mListView.setAdapter(adapter);
-        MyClickListener myClickListener = new MyClickListener();
-        mListView.setOnItemClickListener(myClickListener);
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        MainMenuFragment fragment = new MainMenuFragment();
+        ft.add(R.id.main_container, fragment);
+        ft.commit();
     }
 
 
@@ -65,25 +49,4 @@ public class MainActivity extends RoboFragmentActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    public class MyClickListener implements AdapterView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-            switch (i) {
-                case KINEMATICS:
-                case FORCES:
-                case DYNAMICS:
-                case WORK:
-                    FragmentManager fm = getSupportFragmentManager();
-                    FragmentTransaction ft = fm.beginTransaction();
-                    SubMenuFragment fragment = SubMenuFragment.newInstance("Forces");
-                    ft.replace(R.id.main_menu, fragment);
-                    ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE);
-                    ft.addToBackStack(null);
-                    ft.commit();
-                    //fragment.show(fm, "SubMenuFragment");
-                    break;
-            }
-        }
-    }
 }
---
+++ app/src/main/java/com/physicsformulae/app/MainMenuFragment.java
@@ -0,0 +1,67 @@
+package com.physicsformulae.app;
+
+import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+import roboguice.fragment.RoboFragment;
+import roboguice.inject.InjectResource;
+import roboguice.inject.InjectView;
+
+/**
+ * Created by Wilson on 6/22/14.
+ */
+public class MainMenuFragment extends RoboFragment {
+
+    private final int KINEMATICS = 0;
+    private final int FORCES = 1;
+    private final int DYNAMICS = 2;
+    private final int WORK = 3;
+
+    @InjectView(R.id.main_list)
+    ListView mListView;
+
+    @InjectResource(R.array.topic_list)
+    String[] mListOfTopics;
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final MyAdapter adapter = new MyAdapter(mListOfTopics);
+        mListView.setAdapter(adapter);
+        MyClickListener myClickListener = new MyClickListener();
+        mListView.setOnItemClickListener(myClickListener);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.main_menu_fragment, container, false);
+    }
+
+    public class MyClickListener implements AdapterView.OnItemClickListener {
+
+        @Override
+        public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+            switch (i) {
+                case KINEMATICS:
+                case FORCES:
+                case DYNAMICS:
+                case WORK:
+                    FragmentManager fm = getFragmentManager();
+                    FragmentTransaction ft = fm.beginTransaction();
+                    ft.setCustomAnimations(R.anim.slide_in_right, R.anim.slide_out_left, android.R.anim.slide_in_left, android.R.anim.slide_out_right);
+                    SubMenuFragment fragment = SubMenuFragment.newInstance("Forces");
+                    ft.replace(R.id.main_container, fragment);
+                    ft.addToBackStack(null);
+                    ft.commit();
+                    break;
+            }
+        }
+    }
+}
